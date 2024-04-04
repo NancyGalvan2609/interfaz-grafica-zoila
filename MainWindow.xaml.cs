c860b9b93b6fd02876e9c26e231778516a0fc5fe
CanarioR <diego.sanchez5706@alumnos.udg.mx>
2024-04-04 00:11:52 -0600

    "Nueva actualizacion"

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
 
 namespace interfaz_grafica_zoila
 {
@@ -43,7 +45,36 @@ namespace interfaz_grafica_zoila
 
         private void Btn_Login_Click(object sender, RoutedEventArgs e)
         {
+            string connectionString = "server=localhost ;database=empresa;uid=root;password=root;";
 
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    string query = "SELECT COUNT(*) FROM Empleado WHERE Nombre = @nombre AND Contraseña = @contraseña";
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    cmd.Parameters.AddWithValue("@nombre", txtUsername.Text);
+                    cmd.Parameters.AddWithValue("@contraseña", txtPassword.Password);
+
+                    int count = Convert.ToInt32(cmd.ExecuteScalar());
+
+                    if (count > 0)
+                    {
+                        MessageBox.Show("Inicio de sesión exitoso!");
+                        // Aquí puedes abrir la nueva ventana o realizar otras acciones después del inicio de sesión exitoso
+                    }
+                    else
+                    {
+                        MessageBox.Show("Nombre de usuario o contraseña incorrectos");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al intentar iniciar sesión: " + ex.Message);
+                }
+            }
         }
     }
 }
