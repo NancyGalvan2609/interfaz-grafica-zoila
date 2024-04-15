72a2e2eab2e1a64fd999ef03b35ff63a9e4564b3
Nancy Galvan <nancymichelle2609@gmail.com>
2024-04-15 00:16:14 -0600

    Creacion de ventana Admin, cambio de logos

---
+++ Admin.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace interfaz_grafica_zoila
+{
+    /// <summary>
+    /// Lógica de interacción para Admin.xaml
+    /// </summary>
+    public partial class Admin : Window
+    {
+        public Admin()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnHome_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            AltaUsuariosGrid.Visibility = Visibility.Hidden;
+            GridHome.Visibility = Visibility.Visible;
+        }
+
+        private void AltaUsuarios_Click(object sender, RoutedEventArgs e)
+        {
+            GridHome.Visibility = Visibility.Hidden;
+            AltaUsuariosGrid.Visibility = Visibility.Visible;
+        }
+
+        private void CerrarSesion_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+           
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
+    }
+}
