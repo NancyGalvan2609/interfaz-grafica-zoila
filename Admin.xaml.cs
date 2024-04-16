7ee4ccf3c660d627f827bc1ce2ce7a5862fff326
Nancy Galvan <nancymichelle2609@gmail.com>
2024-04-15 23:50:49 -0600

    Cambios admin

--- Admin.xaml.cs
+++ Admin.xaml.cs
@@ -23,7 +23,31 @@ namespace interfaz_grafica_zoila
         {
             InitializeComponent();
         }
+        //Movimiento de la pantalla
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+        //Minimizar la pantalla 
+        private void MinimizeWindow_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        //Cerrar panralla
+        private void CloseWindow_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
 
+        private void CerrarSesion_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
+        //Regresa al grid principal 
         private void BtnHome_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             AltaUsuariosGrid.Visibility = Visibility.Hidden;
@@ -34,14 +58,45 @@ namespace interfaz_grafica_zoila
         {
             GridHome.Visibility = Visibility.Hidden;
             AltaUsuariosGrid.Visibility = Visibility.Visible;
+            TxtNombre.GotFocus += TxtGotFocus;
         }
 
-        private void CerrarSesion_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void TxtNombre_KeyDown(object sender, KeyEventArgs e)
         {
-           
-            MainWindow main = new MainWindow();
-            main.Show();
-            this.Close();
+            if (e.Key == Key.Enter)
+            {
+                TextBox textBox = (TextBox)sender;
+            }
         }
+
+        private void BtnGuardarInfoNewUsuario_Click(object sender, RoutedEventArgs e)
+        {  
+            MessageBox.Show("oki-doki");
+            
+
+        }
+
+       private void TxtGotFocus(object sender, RoutedEventArgs e)
+        {
+            TxtNombre.Clear();
+        }
+
+
+
+        /*private void TxtNombre_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Verificar si ambas TextBox tienen texto
+            if (!string.IsNullOrEmpty(TxtNombre.Text) && !string.IsNullOrEmpty(textBox2.Text))
+            {
+                // Si ambas están llenas, aumentar la opacidad del botón a 1 (completamente visible)
+                BtnGuardarInfoNewUsuario.Opacity = 1.0;
+            }
+            else
+            {
+                // Si alguna está vacía, reducir la opacidad del botón a 0.5 (semi-transparente)
+                BtnGuardarInfoNewUsuario.Opacity = 0.5;
+            }
+        }*/
+
     }
 }
