c4b85e10ae65f133ce63c0e30c56805aa7a0bde6
Nancy Galvan <nancymichelle2609@gmail.com>
2024-04-15 22:12:24 -0600

    Creación de ventanas de diferentes usuarios

---
+++ Lider.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace interfaz_grafica_zoila
+{
+    /// <summary>
+    /// Lógica de interacción para Lider.xaml
+    /// </summary>
+    public partial class Lider : Window
+    {
+        public Lider()
+        {
+            InitializeComponent();
+        }
+    }
+}
