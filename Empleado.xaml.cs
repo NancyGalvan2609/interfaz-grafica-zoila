87c5a0167e0cae4fd2e0dbf8f66453e76be26b9f
Nancy Galvan <nancymichelle2609@gmail.com>
2024-04-18 07:40:44 -0600

    Ventana Empleado

--- Empleado.xaml.cs
+++ Empleado.xaml.cs
@@ -23,5 +23,84 @@ namespace interfaz_grafica_zoila
         {
             InitializeComponent();
         }
+
+        private void CerrarSesion_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
+
+        private void BtnHome_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        private void CloseWindow_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MinimizeWindow_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        //Permite mover libremente por la pantalla
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void Nomina_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Manuales_Click(object sender, RoutedEventArgs e)
+        {
+            Home.Visibility = Visibility.Hidden;
+
+        }
+
+        private void Chat_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Horario_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Manuales_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Objetivos_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnTabla_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnPromo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnInventario_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnComida_Click(object sender, RoutedEventArgs e)
+        {
+            MyUserControl.Visibility = Visibility.Visible;
+        }
     }
 }
